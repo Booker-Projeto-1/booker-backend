74f8942738d30dec715428bf4794cfb3b1c75aa5
Nicolas MOreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2023-04-17 10:27:15 -0300

    Add: project basic directories structure

---
+++ booker/src/test/java/com/example/booker/BookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
