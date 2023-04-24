c8dc08c434bbf2b05ba41f936d14318cc7138412
Henrique Lopes <henriquelopees7@gmail.com>
2023-04-24 13:45:14 -0300

    feat: Adiciona validação para email duplicado

--- booker/src/main/java/com/ufcg/booker/CreateUserController.java
+++ booker/src/main/java/com/ufcg/booker/CreateUserController.java
@@ -1,6 +1,7 @@
 package com.ufcg.booker;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,14 +9,20 @@ import java.util.Map;
 @RestController
 public class CreateUserController {
 
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    public CreateUserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/users")
-    public Map<String, Object> createUser(@RequestBody CreateUserRequest request) {
+    public ResponseEntity<Map<String, Object>> createUser(@RequestBody CreateUserRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            return ResponseEntity.badRequest().body(Map.of("erro", "Usuário já existente"));
+        }
         User user = new User(request.email(), request.fullName(), request.phoneNumber(), request.password());
         User savedUser = userRepository.save(user);
-        return Map.of("id", savedUser.getId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("id", savedUser.getId()));
     }
 }
--- booker/src/main/java/com/ufcg/booker/UserRepository.java
+++ booker/src/main/java/com/ufcg/booker/UserRepository.java
@@ -3,4 +3,5 @@ package com.ufcg.booker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
