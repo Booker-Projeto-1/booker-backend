72dda3cafa958d6af9200078be477be6b2832625
Henrique Lopes <henriquelopees7@gmail.com>
2023-05-06 19:59:52 -0300

    Allow CORS

--- booker/src/main/java/com/ufcg/booker/BookerApplication.java
+++ booker/src/main/java/com/ufcg/booker/BookerApplication.java
@@ -3,9 +3,12 @@ package com.ufcg.booker;
 import com.ufcg.booker.security.LoggedUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -19,4 +22,14 @@ public class BookerApplication {
 	public String hello(@AuthenticationPrincipal LoggedUser loggedUser) {
 		return String.format("Hello %s!", loggedUser.get().getEmail());
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**").allowedOrigins("*");
+            }
+        };
+    }
 }
