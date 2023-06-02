6adad23b1b89a30e2cc2c93fce5fc0c8db7a016e
Nicolas Moreira Nobre Leite <nicolas.leite@ccc.ufcg.edu.br>
2023-06-02 17:59:57 -0300

    Ajeita CORS para problemas no frontend

--- booker/src/main/java/com/ufcg/booker/security/SecurityConfiguration.java
+++ booker/src/main/java/com/ufcg/booker/security/SecurityConfiguration.java
@@ -76,11 +76,12 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+
         configuration.setMaxAge(3600L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
